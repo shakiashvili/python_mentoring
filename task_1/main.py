cac8c35b10a62a164c8419fefeb416aeeb3ea405
Giorgi Shakiashvili <giorgi_shakiashvili@epam.com>
2025-05-13 06:52:53 +0000

    Update main.py

--- task_1/main.py
+++ task_1/main.py
@@ -13,16 +13,18 @@ def read_json_file(data: str) -> dict:
 cur = datetime.datetime.today()
 
 
-def search_age_minors(data):
+def search_age_minors(data) -> list:
+    minors = []
+    cur = datetime.datetime.today()
     for index, value in enumerate(data):
         x = data[index]['birth_date']
         x = datetime.datetime.strptime(x, '%Y-%m-%d')
         if (cur.year-x.year) < 18:
-            print(data[index])
+            minors.append(data[index])
+    return minors
 
 
 if __name__ == "__main__":
     data = read_json_file('json_program1.json')
     minors = search_age_minors(data)
-
-
+    
