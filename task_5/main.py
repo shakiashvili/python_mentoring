346b250f2244459f1f09c5f0fdefbfe0d578d9a6
Giorgi Shakiashvili <giorgi_shakiashvili@epam.com>
2025-05-29 18:48:59 +0400

    Corrected the logic of the database manager class

--- task_5/main.py
+++ task_5/main.py
@@ -1,28 +1,24 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from schema import Base
-from schema import FilmsDatabaseManager as Films
-
-
-engine = create_engine('sqlite:///films.db')
-Base.metadata.create_all(engine) 
+from schema import Films, FilmsDatabaseManager
 
 
 def main() -> None:
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    Films.create(db_session=session, title='Mimino',
-                 director='Giorgi Danelia', release_year=1977)
-    Films.create(db_session=session, title='Lord of the rings', 
-                 director='Peter Jackson', release_year=2003)
-    Films.create(db_session=session, title='Father of soldier',
-                 director='Revaz Chkheidze', release_year=1965)
-
-    Films.update(db_session=session, film_id=1, title='Mmino')
-    Films.get_all(db_session=session)
-    Films.delete(db_session=session)
-    session.close()
+    try:
+        manager = FilmsDatabaseManager()
+        film1 = Films(title='Mimino', director='Giorgi Danelia',
+                      release_year=1977)
+        film2 = Films(title='Lord of the rings', director='Peter Jackson',
+                      release_year=2003)
+        film3 = Films(title='Father of soldier', director='Revaz Chkheidze',
+                      release_year=1965)
+        manager.add_film(film1)
+        manager.add_film(film2)
+        manager.add_film(film3)
+        manager.update_film(film1, title='ANY TITLE')
+        manager.get_films()
+        manager.delete_films()
+        manager.get_films()
+    finally:
+        manager.close_session()
 
 
 main()
--- task_5/schema.py
+++ task_5/schema.py
@@ -1,41 +1,60 @@
-from sqlalchemy import Column, Integer, String, text, delete
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
 
 
-class FilmsDatabaseManager(Base):
+class Films(Base):
     __tablename__ = 'films'
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     title = Column(String)
     director = Column(String)
     release_year = Column(Integer)
 
     def __repr__(self):
-        return f"""Film: {self.title}, Director: {self.director}, 
-        Release Year {self.release_year}, ID: {self.id}"""
-    
-    @classmethod
-    def get_all(cls, db_session) -> str:
-        list_of_films = db_session.query(cls).all()
-        for i in list_of_films:
-            print(i)
-    
-    @classmethod
-    def create(cls, db_session, **kwargs) -> None:
-        new_film = cls(**kwargs)
-        db_session.add(new_film)
-        db_session.commit()
-    
-    @classmethod
-    def delete(cls, db_session) -> None:
-        db_session.execute(delete(cls))
-        db_session.commit()
+        return f"""<Film(id={self.id}, title='{self.title}',
+        director='{self.director}',
+        release_year={self.release_year})>"""
 
-    @classmethod 
-    def update(cls, db_session, film_id: int, **kwargs) -> None:
-        if not kwargs:
-            print('No argument is passed')
-            return None
-        db_session.query(cls).filter(cls.id == film_id).update(kwargs)
-        db_session.commit()
+
+class FilmsDatabaseManager:
+    def __init__(self):
+        self.engine = create_engine('sqlite:///films.db')
+        Base.metadata.create_all(self.engine)
+        self.Session = sessionmaker(self.engine)
+        self.session = self.Session()
+
+    def add_film(self, film: Films) -> None:
+        self.session.add(film)
+        self.session.commit()
+        return film
+
+    def get_films(self) -> list:
+        films_list = self.session.query(Films).all()
+        for film in films_list:
+            print(film)
+        if not films_list:
+            print(None)
+
+    def delete_films(self) -> None:
+        self.session.query(Films).delete()
+        self.session.commit()
+        print("All Films are deleted")
+
+    def update_film(self, film: Films, **kwargs) -> None:
+        to_be_updated_film = self.__search_film_id(film)
+        if to_be_updated_film:
+            for key, value in kwargs.items():
+                setattr(to_be_updated_film, key, value)
+            self.session.commit()
+            return to_be_updated_film
+
+    def __search_film_id(self, film: Films) -> None:
+        return self.session.query(Films).filter_by(
+            title=film.title, director=film.director,
+            release_year=film.release_year).first()
+
+    def close_session(self) -> None:
+        self.session.close()
